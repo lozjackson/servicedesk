d69770f4cc0b27d27eea9db9d34676667c472777
Loz Jackson <lozjackson@talktalk.net>
2016-02-15 22:27:51 +0000

    update person adapter

--- app/adapters/person.js
+++ app/adapters/person.js
@@ -1,4 +1,36 @@
+/**
+  @module service-desk
+*/
 import ApplicationAdapter from './application';
 
+/**
+  @class PersonAdapter
+  @extends Adapters.ApplicationAdapter
+  @namespace Adapters
+*/
 export default ApplicationAdapter.extend({
+
+  /**
+    Determines the pathname for a given type.
+    By default, it pluralizes the type's name (for example,
+    'post' becomes 'posts' and 'person' becomes 'people').
+    ### Pathname customization
+    For example if you have an object LineItem with an
+    endpoint of "/line_items/".
+    ```app/adapters/application.js
+    import DS from 'ember-data';
+    export default DS.RESTAdapter.extend({
+      pathForType: function(modelName) {
+        var decamelized = Ember.String.decamelize(modelName);
+        return Ember.String.pluralize(decamelized);
+      }
+    });
+    ```
+    @method pathForType
+    @param {String} modelName
+    @return {String} path
+  **/
+  pathForType: function(/*modelName*/) {
+    return 'UserInformationList';
+  }
 });
--- tests/unit/adapters/person-test.js
+++ tests/unit/adapters/person-test.js
@@ -1,12 +1,15 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('adapter:person', 'Unit | Adapter | person', {
-  // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   let adapter = this.subject();
   assert.ok(adapter);
+});
+
+test('pathForType() method', function(assert) {
+  let adapter = this.subject();
+  assert.equal(adapter.pathForType(), 'UserInformationList');
 });
