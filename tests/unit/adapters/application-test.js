6dd6137ddda912314cd4220683165d9780a97717
Loz Jackson <lozjackson@talktalk.net>
2016-02-18 01:28:15 +0000

    update application adapter test
    test for store.query method - implemented in ember-odata adapter

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -1,5 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 import ENV from '../../../config/environment';
+import Ember from 'ember';
 
 moduleFor('adapter:application', 'Unit | Adapter | application', {
 });
@@ -12,4 +13,41 @@ test('host should be ENV.host', function(assert) {
 test('namespace should be ENV.namespace', function(assert) {
   let adapter = this.subject();
   assert.equal(adapter.get('namespace'), ENV.namespace);
+});
+
+test('query() method', function (assert) {
+  assert.expect(4);
+  let adapter = this.subject();
+  adapter.setProperties({
+    namespace: 'api/server.php',
+    ajax: (url, method, obj) => {
+      assert.equal(url, '/api/server.php/people?test=1');
+      assert.equal(method, 'GET');
+      assert.equal(typeof obj, 'undefined');
+    }
+  });
+  let modelType = Ember.Object.create({
+    modelName: 'person'
+  });
+  adapter.query(null, modelType, { test: 1 });
+  assert.ok(adapter);
+});
+
+test('query() method - add to existing query params', function (assert) {
+  assert.expect(4);
+  let adapter = this.subject();
+  adapter.setProperties({
+    queryStringParams: ['$select=Id,Name,Person/id', '$expand=Person'],
+    namespace: 'api/server.php',
+    ajax: (url, method, obj) => {
+      assert.equal(url, '/api/server.php/people?$select=Id,Name,Person/id&$expand=Person&test=1');
+      assert.equal(method, 'GET');
+      assert.equal(typeof obj, 'undefined');
+    }
+  });
+  let modelType = Ember.Object.create({
+    modelName: 'person'
+  });
+  adapter.query(null, modelType, { test: 1 });
+  assert.ok(adapter);
 });
