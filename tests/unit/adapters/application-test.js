f3021a310734d404fe73146223cad0446b945408
Loz Jackson <lozjackson@talktalk.net>
2016-02-22 22:13:49 +0000

    update application adapter test

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -15,6 +15,27 @@ test('namespace should be ENV.namespace', function(assert) {
   assert.equal(adapter.get('namespace'), ENV.namespace);
 });
 
+test('_buildQueryString', function (assert) {
+  let adapter = this.subject();
+
+  assert.equal(adapter._buildQueryString({ test: 'Abc' }), 'test=Abc');
+
+  let query = {
+    anotherKey: `A longer string with spaces and 'other' ch@racters/and $tuff!!`,
+    $key: 2,
+    oneMoreForLuck: 'go on then'
+  };
+  assert.equal(adapter._buildQueryString(query), `anotherKey=A%20longer%20string%20with%20spaces%20and%20'other'%20ch@racters/and%20$tuff!!&$key=2&oneMoreForLuck=go%20on%20then`);
+});
+
+test('_appendQueryString', function (assert) {
+  let adapter = this.subject();
+  let queryString = 'testKey=1&anotherKey=abc';
+  let query = { testKey: 1, anotherKey: 'abc' };
+  assert.equal(adapter._appendQueryString('example.com/page', query), `example.com/page?${queryString}`);
+  assert.equal(adapter._appendQueryString('example.com/page?firstKey=test', query), `example.com/page?firstKey=test&${queryString}`);
+});
+
 test('query() method', function (assert) {
   assert.expect(4);
   let adapter = this.subject();
@@ -49,5 +70,23 @@ test('query() method - add to existing query params', function (assert) {
     modelName: 'person'
   });
   adapter.query(null, modelType, { test: 1 });
+  assert.ok(adapter);
+});
+
+test('queryRecord() method', function (assert) {
+  assert.expect(4);
+  let adapter = this.subject();
+  adapter.setProperties({
+    namespace: 'api/server.php',
+    ajax: (url, method, obj) => {
+      assert.equal(url, '/api/server.php/people?test=1');
+      assert.equal(method, 'GET');
+      assert.equal(typeof obj, 'undefined');
+    }
+  });
+  let modelType = Ember.Object.create({
+    modelName: 'person'
+  });
+  adapter.queryRecord(null, modelType, { test: 1 });
   assert.ok(adapter);
 });
