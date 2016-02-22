8833f50adea27382c159f5a161bc0202c45caf81
Loz Jackson <lozjackson@talktalk.net>
2016-02-22 19:55:20 +0000

    update index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,33 +9,44 @@ import Ember from 'ember';
 */
 export default Ember.Route.extend({
 
+  /**
+    @property queryParams
+    @type {Object}
+  */
   queryParams: {
     status: {
       refreshModel: true
     }
   },
+
   /**
-    findAll `job`
+    ## Model
+
+    Query `job` models (apply these query params).
+
+    * $select: `Id,Modified,Title,StatusValue,ProblemDescription,AssignedTo/Id,Requester/Id`,
+    * $expand: `AssignedTo,Requester`,
+    * $filter: `substringof('${this.user.email}',OriginalAssignee) and StatusValue eq '${status}'`
+
+    `this.user.email` is the current user's email, and `status` is the value of the
+    `status` query param in the URL.
 
     @method model
-    @return {Array}
+    @param {Object} params
+    @return {Array} An array of `JobModel`s
   */
   model(params) {
-    // let id = this.get('user.id');
-    // return id ? this.store.query('job', { $filter: `AssignedTo/Id eq ${id}`}) : this.store.findAll('job');
-
-    let select = 'Id,Modified,Title,StatusValue,ProblemDescription,AssignedTo/Id,Requester/Id';
-    let expand = 'AssignedTo,Requester';
     let status = params.status || 'Active';
-    let email = this.get('user.email');
     let filter = `StatusValue eq '${status}'`;
+    let email = this.get('user.email');
+
     if (email) {
-      filter = `substringof('${email}',OriginalAssignee) and ` + filter;
+      filter = `substringof('${email}',OriginalAssignee) and ${filter}`;
     }
 
     return this.store.query('job', {
-      $select: select,
-      $expand: expand,
+      $select: 'Id,Modified,Title,StatusValue,ProblemDescription,AssignedTo/Id,Requester/Id',
+      $expand: 'AssignedTo,Requester',
       $filter: filter
     });
   }
--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -1,4 +1,5 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('route:index', 'Unit | Route | index', {
   needs: [
@@ -7,18 +8,40 @@ moduleFor('route:index', 'Unit | Route | index', {
   ]
 });
 
-test('model() method should findAll jobs', function(assert) {
-  assert.expect(2);
+test('queryParams', function (assert) {
+    var route = this.subject();
+    let queryParams = route.get('queryParams');
+    let keys = Object.keys(queryParams);
+    assert.ok(queryParams);
+    assert.equal(keys.length, 1);
+    assert.equal(keys[0], 'status');
+    assert.equal(Object.keys(route.get('queryParams.status')).length, 1);
+    assert.equal(Object.keys(route.get('queryParams.status'))[0], 'refreshModel');
+    assert.equal(route.get('queryParams.status.refreshModel'), true);
+});
+
+test('model() method should send query', function(assert) {
+  assert.expect(5);
   var route = this.subject();
   let _findAll = route.store.findAll;
+  let _query = route.store.query;
+  route.set('user', Ember.Object.create({ email: 'test.user@example.com' }));
 
-  route.store.findAll = function (modelName) {
+  route.store.findAll = function () {
+    assert.ok(false, `'findAll() should not be called'`);
+  };
+
+  route.store.query = function (modelName, query) {
     assert.equal(modelName, 'job');
+    assert.equal(query.$select, 'Id,Modified,Title,StatusValue,ProblemDescription,AssignedTo/Id,Requester/Id');
+    assert.equal(query.$expand, 'AssignedTo,Requester');
+    assert.equal(query.$filter, `substringof('test.user@example.com',OriginalAssignee) and StatusValue eq 'Active'`);
     return [ {id:1}, {id:2} ];
   };
 
-  let jobs = route.model();
+  let jobs = route.model({status:'Active'});
   assert.equal(jobs.get('length'), 2);
 
   route.store.findAll = _findAll;
+  route.store.query = _query;
 });
